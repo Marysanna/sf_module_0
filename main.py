806fae87b7045dd7f67ad5cb04371d27be6f657c
Nikita Larkin <you@example.com>
2020-02-16 14:26:05 +0500

    all work

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello world")
+import numpy as np
+
+number = np.random.randint(1,100)
+count = 0
+predict = 50
+high_border = 100
+low_border = 1
+while number != predict:
+    predict = int ((high_border + low_border)/2)
+    count+=1
+    if number > predict: 
+        low_border = predict
+    elif number < predict:
+        high_border = predict
+    else:
+        break
+print("Компьютер угадал число", number, "за", count, "попыток")
