7a49afe680334b74a234caeca1f0e4f5c919507d
Nikita Larkin <you@example.com>
2020-03-06 12:07:41 +0500

    Added comments

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-number = np.random.randint(1,100)
-count = 0
-predict = 50
-high_border = 100
+high_border = 100                                  # Можно через int(input())                      
 low_border = 1
-while number != predict:
-    predict = int ((high_border + low_border)/2)
+number = np.random.randint(low_border,high_border) # Компьютер загадывает номер от 1 до 100
+count = 0                                          # Счётчик попыток     
+
+while number != predict:                           # Цикл который повторяется пока программа не угадает номер 
+    predict = int ((high_border + low_border)/2)   # Предположительное число равно половине интервала
     count+=1
-    if number > predict: 
+    if number > predict:                           # При неверном предположении границы возможных чисел сужаются
         low_border = predict
     elif number < predict:
         high_border = predict
